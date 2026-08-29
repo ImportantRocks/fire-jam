e93a41c24e8fe26b1cf9960cfc32106a232c1dc0
Important Rocks <dcgaechter@gmail.com>
2026-08-29 15:06:59 -0700

    Dialog scripting working well now

--- MainScript.gd
+++ MainScript.gd
@@ -8,40 +8,71 @@ var currentTimer
 func _ready() -> void:
 	#Starting the game with the first dialog
 	get_tree().root.get_node("/root/Main//Dialog").add_child(DialogDb.dialog0)
+	
 	#set animation and timer ref as variables
 	currentAnim = DialogDb.dialog0.get_node("AnimationPlayer")
 	currentTimer = DialogDb.dialog0.get_node("Timer")
-	#connect end animation signal to start timer
-	currentAnim.animation_finished.connect(dialog1)
 	
-	#connect next dialog to timer timeout signal
-	#timer.timeout.connect(dialog1)
+	#connect end animation signal to start timer
+	currentAnim.animation_finished.connect(dialog0TimerStart)
 
-func dialog1(_string):
-	#disconnect from signal
-	currentAnim.animation_finished.disconnect(dialog1)
+func dialog0TimerStart(_string):
+	currentTimer.start()
+	#disconnect signal from animation to timer
+	currentAnim.animation_finished.disconnect(dialog0TimerStart)
+	#connect next dialog to timer timeout signal
+	currentTimer.timeout.connect(dialog1)
+
+
+
+
+func dialog1():
+	#disconnect from previous timer signal
+	currentTimer.timeout.disconnect(dialog1)
+	
 	#remove previous dialog child
 	get_tree().root.get_node("/root/Main/Dialog").remove_child(DialogDb.dialog0)
 	#add new dialog child
 	get_tree().root.get_node("/root/Main//Dialog").add_child(DialogDb.dialog1)
+	
 	#set animation ref as currentAnim
 	currentAnim = DialogDb.dialog1.get_node("AnimationPlayer")
-	#connect to next dialog
-	currentAnim.animation_finished.connect(dialog2)
+	currentTimer = DialogDb.dialog1.get_node("Timer")
+	
+	#connect end animation signal to start timer
+	currentAnim.animation_finished.connect(dialog1TimerStart)
+	
+func dialog1TimerStart(_string):
+	currentTimer.start()
+	#disconnect signal from animation to timer
+	currentAnim.animation_finished.disconnect(dialog1TimerStart)
+	#connect next dialog to timer timeout signal
+	currentTimer.timeout.connect(dialog2)
 
 
 
-func dialog2(_string):
-	#disconnect from signal
-	currentAnim.animation_finished.disconnect(dialog2)
+func dialog2():
+	#disconnect from previous timer signal
+	currentTimer.timeout.disconnect(dialog2)
+	
 	#remove previous dialog child
 	get_tree().root.get_node("/root/Main/Dialog").remove_child(DialogDb.dialog1)
 	#add new dialog child
 	get_tree().root.get_node("/root/Main//Dialog").add_child(DialogDb.dialog2)
+	
 	#set animation ref as currentAnim
 	currentAnim = DialogDb.dialog1.get_node("AnimationPlayer")
-	#connect to next dialog
-	#currentAnim.animation_finished.connect(dialog2)
+	currentTimer = DialogDb.dialog1.get_node("Timer")
+	
+	#connect end animation signal to start timer
+	currentAnim.animation_finished.connect(dialog2TimerStart)
+
+func dialog2TimerStart(_string):
+	currentTimer.start()
+	#disconnect signal from animation to timer
+	currentAnim.animation_finished.disconnect(dialog2TimerStart)
+	#connect next dialog to timer timeout signal
+	#currentTimer.timeout.connect(dialog2)
 	
 	
 	
