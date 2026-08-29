7fa2b5e52bfc0c8f58a6d99e015c2a955909f76c
StrawberryEclair <HopeArmstrong1204@gmail.com>
2026-08-29 01:09:32 -0700

    basic player cam setup

---
+++ animatable_body_3d.gd
@@ -0,0 +1,24 @@
+extends AnimatableBody3D
+
+@export var look_sensitivity: float = 0.005
+
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		head.rotate_y(-event.relative.x * look_sensitivity)
+		camera.rotate_x(-event.relative.y * look_sensitivity)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-80), deg_to_rad(80))
+
+# Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
