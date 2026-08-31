1c8555f6649b03fae88a1461ceabc9df701f650c
StrawberryEclair <HopeArmstrong1204@gmail.com>
2026-08-31 02:47:01 -0700

    Made logs a depletable resource. Currently not shown visually

--- Interact/interactable.gd
+++ Interact/interactable.gd
@@ -7,7 +7,18 @@ signal interacted(body)
 @export var prompt_message = 'Interact'
 @export var prompt_input = 'interact'
 
+	
 func interact(body):
 	print(body.name, " interacted with ", name)
-	interacted.emit(body)
-	Global.fireup.emit()
+	
+	if Global.logs_remaining >= 1:
+		interacted.emit(body)
+		Global.log_added.emit()
+		Global.logs_remaining -= 1
+		print(" Logs remaining = ", Global.logs_remaining)
+			
+	else:
+		Global.logs_remaining = 0
+		print(" ...but there was nothing left to burn ")
+			
+	
--- Scripts/GlobalAutoload.gd
+++ Scripts/GlobalAutoload.gd
@@ -1,8 +1,10 @@
 extends Node
 
-# 0 = The fire is dead, 1 = Weak fire, 2 = Medium fire, 3 = Strong fire
-# Fire scripting is handled mainly in fire.gd
-var firelevel = 2
+
+# Fire scripting is handled in fire.gd
+var fire_level = 2
+
+var logs_remaining = 10
 
 # Sends a signal that the fire has been fed and should increase 1 level.
-signal fireup()
+signal log_added()
